72260dcc4858a96ee770929c8ef138e10102f9e0
Tradeobrih <romera.valladolid@gmail.com>
2024-11-07 22:04:26 +0000

    First Commit del 07/11/2024

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 import { Digit } from "./Digit";
 
-const Home = ({ counter }) => {
-  const digitsArray = counter.toString().padStart(6, "0").split(""); 
+const Home = ({ counter, startTimer, stopTimer, resetTimer, formatLength = 6, color = "limegreen" }) => {
+  const digitsArray = counter.toString().padStart(formatLength, "0").split("");
 
   return (
-    <div className="clock">
-      <span className="fi fi-rr-clock"></span>
-      {digitsArray.map((digit, index) => (
-        <Digit 
-          key={index} 
-          digit={digit} 
-          digitStyle={{ color: "limegreen" }} 
-        />
-      ))}
+    <div>
+      <div className="clock">
+        <span className="fi fi-rr-clock"></span>
+        {digitsArray.map((digit, index) => (
+          <Digit key={index} digit={digit} digitStyle={{ color }} />
+        ))}
+      </div>
+      <div className="buttons">
+        <button className="btn btn-primary" onClick={startTimer}>Iniciar</button>
+        <button className="btn btn-secondary mx-2" onClick={stopTimer}>Detener</button>
+        <button className="btn btn-danger" onClick={resetTimer}>Reiniciar</button>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+//mport React from "react";
+//mport { Digit } from "./Digit";
+//
+//onst Home = ({ counter }) => {
+// const digitsArray = counter.toString().padStart(6, "0").split(""); 
+//
+// return (
+//   <div className="clock">
+//     <span className="fi fi-rr-clock"></span>
+//     {digitsArray.map((digit, index) => (
+//       <Digit 
+//         key={index} 
+//         digit={digit} 
+//         digitStyle={{ color: "limegreen" }} 
+//       />
+//     ))}
+//   </div>
+// );
+//;
+//
+//xport default Home;
 
 //import React from "react";
 //import { Digit } from "./Digit";
--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,99 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 import Home from "./component/home.jsx";
 
+let counter = 0;
+let isRunning = false;
+let intervalId = null;
+
+const startTimer = (updateApp) => {
+  if (!isRunning) {
+    isRunning = true;
+    intervalId = setInterval(() => {
+      counter += 1;
+      updateApp(); 
+    }, 1000);
+  }
+};
+
+const stopTimer = () => {
+  isRunning = false;
+  clearInterval(intervalId);
+};
+
+const resetTimer = (updateApp) => {
+  isRunning = false;
+  clearInterval(intervalId);
+  counter = 0;
+  updateApp(); 
+};
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  let intervalId = null;
-
-  useEffect(() => {
-    if (isRunning) {
-      intervalId = setInterval(() => {
-        setCounter(prevCounter => prevCounter + 1);
-      }, 1000);
-    }
-
-    return () => clearInterval(intervalId);
-  }, [isRunning]);
-
-  const startTimer = () => setIsRunning(true);
-  const stopTimer = () => setIsRunning(false);
-  const resetTimer = () => {
-    setIsRunning(false);
-    setCounter(0);
-  };
+  const updateApp = () => root.render(<App />);
 
   return (
     <div className="app-container">
-      <Home 
-        counter={counter} 
-        formatLength={6} 
-        color="limegreen" 
+      <Home
+        counter={counter}
+        startTimer={() => startTimer(updateApp)}
+        stopTimer={stopTimer}
+        resetTimer={() => resetTimer(updateApp)}
+        formatLength={6}
+        color="limegreen"
       />
-      <div className="buttons">
-        <button className="btn btn-primary" onClick={startTimer}>Iniciar</button>
-        <button className="btn btn-secondary mx-2" onClick={stopTimer}>Detener</button>
-        <button className="btn btn-danger" onClick={resetTimer}>Reiniciar</button>
-      </div>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.querySelector("#app"));
 root.render(<App />);
+
+//import React, { useState, useEffect } from "react";
+//import ReactDOM from "react-dom/client";
+//import "../styles/index.css";
+//import Home from "./component/home.jsx";
+//
+//const App = () => {
+//  const [counter, setCounter] = useState(0);
+//  const [isRunning, setIsRunning] = useState(false);
+//  let intervalId = null;
+//
+//  useEffect(() => {
+//    if (isRunning) {
+//      intervalId = setInterval(() => {
+//        setCounter(prevCounter => prevCounter + 1);
+//      }, 1000);
+//    }
+//
+//    return () => clearInterval(intervalId);
+//  }, [isRunning]);
+//
+//  const startTimer = () => setIsRunning(true);
+//  const stopTimer = () => setIsRunning(false);
+//  const resetTimer = () => {
+//    setIsRunning(false);
+//    setCounter(0);
+//  };
+//
+//  return (
+//    <div className="app-container">
+//      <Home 
+//        counter={counter} 
+//        formatLength={6} 
+//        color="limegreen" 
+//      />
+//      <div className="buttons">
+//        <button className="btn btn-primary" onClick={startTimer}>Iniciar</button>
+//        <button className="btn btn-secondary mx-2" onClick={stopTimer}>Detener</button>
+//        <button className="btn btn-danger" onClick={resetTimer}>Reiniciar</button>
+//      </div>
+//    </div>
+//  );
+//};
+//
+//const root = ReactDOM.createRoot(document.querySelector("#app"));
+//root.render(<App />);
 
 //import React, { useState, useEffect } from "react";
 //import ReactDOM from "react-dom/client";
